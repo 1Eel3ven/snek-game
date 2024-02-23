341fe90cf145766b0cb58dba1e1808b9aa9d7979
ivank <korichivanV@mail.ru>
2024-02-23 19:21:05 +0700

    Updated window

--- snake_game.py
+++ snake_game.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from random import randint
 
-# ----------------------------------------- constants -----------------------------------
+# ----------------------------------------- Constants -----------------------------------
 
 # sizes
 GAME_WIDTH = 700
@@ -18,7 +18,7 @@ FOOD_COLOR = "#3096e2"
 BG_COLOR = "#3e3e42"
 
 
-# ------------------------------------ classes, funcs ------------------------------------
+# ------------------------------------ Classes, funcs ------------------------------------
 
 class Snake:
     def __init__(self):
@@ -41,7 +41,7 @@ class Food:
 
 
 # direction management
-
+        
 def next_move():
     pass
 
@@ -56,3 +56,39 @@ def check_collisions(snake):
 
 def game_over():
     pass
+
+
+# ----------------------------------------- Main game --------------------------------------
+
+# window settings
+window = Tk()
+window.title("Snake Game")
+window.resizable(False, False)
+
+# default values
+score = 0
+dir = 'DOWN'
+
+
+# screen settings
+label = Label(window, text=f'Score: {score}', font=('comic-sans', 30))
+label.pack()
+
+canvas = Canvas(window, width=GAME_WIDTH, height=GAME_HEIGHT, bg=BG_COLOR)
+canvas.pack()
+
+window.update() # !
+
+# centering the window
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int(screen_width / 2) - int(window_width / 2)
+y = int(screen_height / 2) - int(window_height / 2)
+
+window.geometry(f'{window_width}x{window_height}+{x}+{y}')
+
+
+window.mainloop()
